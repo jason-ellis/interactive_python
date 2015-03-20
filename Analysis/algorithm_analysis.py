204d64460905874addf439a45e8d4f23f358b795
Jason Ellis <jason.c.ellis@gmail.com>
2015-03-20 15:41:14 -0500

    initial commit

---
+++ Analysis/algorithm_analysis.py
@@ -0,0 +1,13 @@
+# http://interactivepython.org/runestone/static/pythonds/AlgorithmAnalysis/WhatIsAlgorithmAnalysis.html
+
+
+# Simple sum function
+def sum_of_n(n):
+    the_sum = 0
+    for i in range(1, n+1):
+        the_sum = the_sum + i
+
+    return the_sum
+
+print(sum_of_n(10))
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Interactive Python
+
+This is a bit of a scratch area for me to include example code from interactivepython.org.
+
+I've already noticed that a lot of the examples are not PEP8 compliant, so I'll be changing mine to comply.

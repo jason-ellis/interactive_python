4c0167e960b8826b2b970ec08eea6854a538d2b4
Jason Ellis <jason.c.ellis@gmail.com>
2015-03-22 12:13:12 -0500

    completed

---
+++ basic_data_structures/linear_structures.py
@@ -0,0 +1,13 @@
+# http://interactivepython.org/runestone/static/pythonds/BasicDS/WhatAreLinearStructures.html
+
+# Stacks, queues, deques, lists
+
+# Items are ordered depending on how they are added or removed
+# Items added stay in position relative to other elements before and after
+
+# Linear structures have two ends:
+# left & right
+# front & rear
+# top & bottom
+
+# Linear structures are differentiated by how/where items are added and removed

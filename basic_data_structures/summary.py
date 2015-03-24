a1fa92bb61f316fd6001fe44eba13bb5e14c3cbd
Jason Ellis <jason.c.ellis@gmail.com>
2015-03-24 14:22:54 -0500

    completed summary

---
+++ basic_data_structures/summary.py
@@ -0,0 +1,31 @@
+# http://interactivepython.org/runestone/static/pythonds/BasicDS/Summary.html
+
+
+# Summary
+
+# Linear data structures maintain their data in an ordered fashion
+
+# Stacks
+# Simple data structures that maintain LIFO ordering
+# Fundamental operations for stack are push, pop, is_empty
+# Useful for designing algorithms to evaluate and translate expressions
+# Can provide reversal characteristic
+
+# Queues
+# Simple data structures that maintain FIFO ordering
+# Fundamental operations for a queue are enqueue, dequeue, and is_empty
+# Can assist in the construction of timing simulations
+# Simulations use random number generators to create a real-life situation
+#   and allow us to answer "what if" questions
+
+# Prefix, infix, and postfix are all ways to write expressions
+
+# Deques
+# Data structures that allow hybrid behavior like that of stack and queues
+# Fundamental ops: add_front, add_rear, remove_front, remove_rear, is_empty
+
+# Lists
+# Collections of items where each item holds a relative position
+# Linked list implementation maintains logical order without requiring
+#   physical storage requirements.
+# Modification to the head of the linked list is a special case.
